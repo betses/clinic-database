d3ea8173031e40168a40526d6fbd3b1c18dad5a6
GordonOtieno <otienogordon95@gmail.com>
2022-10-25 16:02:53 +0300

    Add invoice invoice _items and treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,32 @@ CREATE TABLE "patients"(
     "date_of_birth" DATE NOT NULL,
     ADD PRIMARY KEY("id")
 );
+
+CREATE TABLE "treatments"(
+    "id" INTEGER NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "name" DATE NOT NULL,
+    ADD PRIMARY KEY("id")
+    CONSTRAINT fk_medical_histories FOREIGN KEY(id) REFERENCES medical_histories(id),
+);
+
+CREATE TABLE "invoice_items"(
+    "id" INTEGER NOT NULL,
+    "unit_price" DECIMAL NOT NULL,
+    "quantity" INTEGER,
+    "total_price" INTEGER,
+    "invoice_id" INTEGER,
+    "treatment_id" INTEGER,
+    CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id),
+    CONSTRAINT fk_invoices FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+    ADD PRIMARY KEY("id")
+);
+CREATE TABLE "invoices"(
+    "id" INTEGER NOT NULL,
+    "total_amount" DECIMAL,
+    "generated_at" TIMESTAMP,
+    "paye_at" TIMESTAMP,
+    "medical_history_id" INTEGER,
+    CONSTRAINT fk_invoice_items FOREIGN KEY(id) REFERENCES owners(id),
+    ADD PRIMARY KEY("id")
+);
