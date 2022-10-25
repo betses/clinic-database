f4d1658c4e1c97520a6722e87a0687a95fceb412
GordonOtieno <otienogordon95@gmail.com>
2022-10-25 16:49:05 +0300

    Add join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,24 +1,34 @@
-CREATE TABLE "medical_histories"(
-    "id" INTEGER NOT NULL,
-    "admited_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "patient_id" INTEGER NOT NULL,
-    "status" VARCHAR(255) NOT NULL,
-    ADD PRIMARY KEY("id")
-    CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id),
-);
 CREATE TABLE "patients"(
     "id" INTEGER NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "date_of_birth" DATE NOT NULL,
-    ADD PRIMARY KEY("id")
+     PRIMARY KEY(id)
 );
+CREATE TABLE medical_histories(
+    id INTEGER NOT NULL,
+    admited_at TIMESTAMP,
+    patient_id INTEGER NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id),
+    PRIMARY KEY(id)
+    );
 
 CREATE TABLE "treatments"(
     "id" INTEGER NOT NULL,
     "type" VARCHAR(255) NOT NULL,
     "name" DATE NOT NULL,
-    ADD PRIMARY KEY("id")
-    CONSTRAINT fk_medical_histories FOREIGN KEY(id) REFERENCES medical_histories(id),
+    CONSTRAINT fk_medical_histories FOREIGN KEY("id") REFERENCES medical_histories("id"),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE "invoices"(
+    "id" INTEGER NOT NULL,
+    "total_amount" DECIMAL,
+    "generated_at" TIMESTAMP,
+    "paye_at" TIMESTAMP,
+    "medical_history_id" INTEGER,
+    CONSTRAINT fk_invoice_items FOREIGN KEY("medical_history_id") REFERENCES medical_histories("id"),
+     PRIMARY KEY(id)
 );
 
 CREATE TABLE "invoice_items"(
@@ -28,16 +38,14 @@ CREATE TABLE "invoice_items"(
     "total_price" INTEGER,
     "invoice_id" INTEGER,
     "treatment_id" INTEGER,
-    CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id),
-    CONSTRAINT fk_invoices FOREIGN KEY(invoice_id) REFERENCES invoices(id),
-    ADD PRIMARY KEY("id")
+    CONSTRAINT fk_treatment FOREIGN KEY("treatment_id") REFERENCES treatments("id"),
+    CONSTRAINT fk_invoices FOREIGN KEY("invoice_id") REFERENCES invoices("id"),
+    PRIMARY KEY(id)
 );
-CREATE TABLE "invoices"(
-    "id" INTEGER NOT NULL,
-    "total_amount" DECIMAL,
-    "generated_at" TIMESTAMP,
-    "paye_at" TIMESTAMP,
-    "medical_history_id" INTEGER,
-    CONSTRAINT fk_invoice_items FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
-    ADD PRIMARY KEY("id")
+
+CREATE TABLE treatment_match (
+    medical_history_id int,
+    treatment_id int,
+    CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories("id"),
+    CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments("id")
 );
